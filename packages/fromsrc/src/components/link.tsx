4be3ddad29df28c68c946c419a2729aeb870d7a5
keypad <githubs15@outlook.com>
2026-04-06 20:22:20 +0100

    □ remove contradictory role=img from decorative svg in link

--- packages/fromsrc/src/components/link.tsx
+++ packages/fromsrc/src/components/link.tsx
@@ -48,7 +48,6 @@ function LinkComponent({
           viewBox="0 0 16 16"
           fill="currentColor"
           className="size-3 text-muted"
-          role="img"
         >
           <path d="M3.75 2h3.5a.75.75 0 010 1.5h-2.69l5.72 5.72a.75.75 0 11-1.06 1.06L3.5 4.56v2.69a.75.75 0 01-1.5 0v-3.5A.75.75 0 012.75 3h1z" />
           <path d="M6.25 7.75a.75.75 0 000 1.5h5.69l-5.72 5.72a.75.75 0 101.06 1.06l5.72-5.72v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5z" />
